7b4ae574ca2497ea8922f22a82f77c4c6cf413c8
franktee <could.net@gmail.com>
2020-04-19 23:04:44 +0800

    vector

--- main.cpp
+++ main.cpp
@@ -9,8 +9,8 @@ namespace vs = std::ranges::views;
 namespace rs = std::ranges;
 
 int main() {
-    std::vector<string> v1 {"甲", "乙", "丙", "丁", "戊", "己", "庚", "辛", "壬", "癸"};
-    std::vector<string> v2 {"子", "丑", "寅", "卯", "辰", "巳", "午", "未", "申", "酉", "戌", "亥"};
+    vector v1 {"甲", "乙", "丙", "丁", "戊", "己", "庚", "辛", "壬", "癸"};
+    vector v2 {"子", "丑", "寅", "卯", "辰", "巳", "午", "未", "申", "酉", "戌", "亥"};
 
     auto a = v1 | vs::cycle;
     auto b = v2 | vs::cycle;
