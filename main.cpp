78e27bb3e1aac26d62aec1b150fee4aba707bbc4
franktee <could.net@gmail.com>
2020-04-19 19:56:27 +0800

    done

--- main.cpp
+++ main.cpp
@@ -9,12 +9,15 @@ namespace vs = std::ranges::views;
 namespace rs = std::ranges;
 
 int main() {
-    std::vector<string> in {"甲", "乙", "丙", "丁", "戊", "己", "庚", "辛", "壬", "癸"};
+    std::vector<string> v1 {"甲", "乙", "丙", "丁", "戊", "己", "庚", "辛", "壬", "癸"};
+    std::vector<string> v2 {"子", "丑", "寅", "卯", "辰", "巳", "午", "未", "申", "酉", "戌", "亥"};
 
-    auto a = in | vs::cycle | vs::take(20);
+    auto a = v1 | vs::cycle;
+    auto b = v2 | vs::cycle;
+    auto c = vs::zip(a, b) | vs::take(60);
 
-    for(auto&& i: a)
-    	cout<<i<<" ";
+    for(auto&& [x, y]: c)
+    	cout<<x<<y<<" ";
     cout<<"\n";
 
     return 0;
--- more_ranges.h
+++ more_ranges.h
@@ -133,6 +133,7 @@ private:
 	{
 	private:
 		friend zip_view;
+		friend _Sentinel;
 		using _Vp1_iter = iterator_t<_Vp1>;
 		using _Vp2_iter = iterator_t<_Vp2>;
 
@@ -140,6 +141,10 @@ private:
 		_Vp2_iter _M_current2 = _Vp2_iter();
 		zip_view* _M_parent = nullptr;
 	public:
+		using iterator_category = typename iterator_traits<_Vp1_iter>::iterator_category;
+		using value_type = std::pair<range_value_t<_Vp1>, range_value_t<_Vp2>>;
+		using difference_type = range_difference_t<_Vp1>;
+		
 		_Iterator() = default;
 
 		constexpr _Iterator(zip_view& __parent,
@@ -147,11 +152,94 @@ private:
 			_M_current1(std::move(__current1)),
 			_M_current2(std::move(__current2)),
 			_M_parent(std::__addressof(__parent)) {}
+
+		constexpr std::pair<range_reference_t<_Vp1>, range_reference_t<_Vp2>> 
+			operator*() const
+		{ return std::make_pair(std::ref(*_M_current1), std::ref(*_M_current2)); }
+
+		constexpr std::pair<_Vp1_iter, _Vp2_iter> operator->() const
+			requires __detail::__has_arrow<_Vp1_iter> &&
+					__detail::__has_arrow<_Vp2_iter>
+		{ return std::make_pair(_M_current1, _M_current2); }
+
+		constexpr _Iterator& operator++()
+		{
+			++_M_current1;
+			++_M_current2;
+			//if(_M_current1 == ranges::end(_M_parent->_M_base1)
+			//	|| _M_current2 == ranges::end(_M_parent->_M_base2))
+			return *this;
+		}
+
+		constexpr void operator++(int)
+		{ ++*this; }
+
+		constexpr _Iterator operator++(int) requires forward_range<_Vp1> && forward_range<_Vp2>
+		{
+			auto __tmp = *this;
+			++*this;
+			return __tmp;
+		}
+
+		friend constexpr bool operator==(const _Iterator& __x, const _Iterator& __y)
+			requires equality_comparable<_Vp1_iter> && equality_comparable<_Vp2_iter>
+		{ return __x._M_current1 == __y._M_current1 && __x._M_current2 == __y._M_current2; }
+	};
+
+	struct _Sentinel
+	{
+	private:
+		std::pair<sentinel_t<_Vp1>, sentinel_t<_Vp2>> _M_end = 
+			std::make_pair(sentinel_t<_Vp1>(), sentinel_t<_Vp2>());
+
+		constexpr bool __equal(const _Iterator& __i) const
+		{
+			return __i._M_current1 == _M_end.first || __i._M_current2 == _M_end.second;
+		}
+	public:
+		_Sentinel() = default;
+
+		constexpr explicit _Sentinel(zip_view& __parent): _M_end(std::make_pair(
+			ranges::end(__parent._M_base1), ranges::end(__parent._M_base2))) {}
+
+		friend constexpr bool operator==(const _Iterator& __x, const _Sentinel& __y)
+		{ return __y.__equal(__x); }
 	};
 
 	_Vp1 _M_base1 = _Vp1();
 	_Vp2 _M_base2 = _Vp2();
+public:
+	zip_view() = default;
+
+	constexpr zip_view(_Vp1 __base1, _Vp2 __base2): _M_base1(std::move(__base1)),
+		_M_base2(std::move(__base2)) {}
+
+	constexpr _Iterator begin()
+	{ return {*this, ranges::begin(_M_base1), ranges::begin(_M_base2)}; }
+
+	constexpr auto end()
+	{
+		//if constexpr (common_range<_Vp1> && common_range<_Vp2>)
+		//	return _Iterator{*this, ranges::end(_M_base1), ranges::end(_M_base2)};
+		//else
+			return _Sentinel{*this};
+	}
 };
+
+
+template <input_range _Vp1, input_range _Vp2>
+inline constexpr bool enable_borrowed_range<zip_view<_Vp1, _Vp2>> = true;
+
+template <input_range _Range1, input_range _Range2>
+zip_view(_Range1&&, _Range2&&) -> zip_view<views::all_t<_Range1>, views::all_t<_Range2>>;
+
+namespace views {
+	inline constexpr __adaptor::_RangeAdaptor zip
+	= [] <viewable_range _Range1, viewable_range _Range2> (_Range1&& __r1, _Range2&& __r2)
+	{
+		return zip_view { std::forward<_Range1>(__r1), std::forward<_Range2>(__r2) };
+	};
+} // namespace views
 
 } // namespace ranges
 } // mamespace std
